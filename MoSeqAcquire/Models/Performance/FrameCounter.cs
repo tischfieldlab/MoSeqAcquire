632c963ed26765a9a0525963f7f6a699ff714b46
quantumdot <thackray@rutgers.edu>
2018-03-14 18:25:07 -0400

    some initial work on performance counters, but nothing substantial or working yet.

---
+++ MoSeqAcquire/Models/Performance/FrameCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoSeqAcquire.Models.Performance
+{
+    class FrameCounter
+    {
+        private long __totalCount;
+        private static readonly object lockobject = new object();
+
+        public FrameCounter()
+        {
+
+        }
+        public void Increment()
+        {
+            lock (lockobject)
+            {
+                this.__totalCount++;
+            }
+        }
+
+    }
+}
