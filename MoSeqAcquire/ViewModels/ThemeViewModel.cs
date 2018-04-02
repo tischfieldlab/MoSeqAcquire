a751732f37d2e86ae13c7861ea6c0946ef940e80
quantumdot <thackray@rutgers.edu>
2018-04-02 17:05:59 -0400

    fixed some issues with the new theme view model and command bindings

--- MoSeqAcquire/ViewModels/ThemeViewModel.cs
+++ MoSeqAcquire/ViewModels/ThemeViewModel.cs
@@ -19,13 +19,21 @@ namespace MoSeqAcquire.ViewModels
     {
         protected PaletteHelper helper;
         protected LightDarkMode lightDarkMode;
+        protected Swatch primarySwatch;
+        protected Swatch accentSwatch;
 
         public ThemeViewModel()
         {
-            Swatches = new SwatchesProvider().Swatches;
             this.helper = new PaletteHelper();
+
+            this.SetThemeCommand = new ActionCommand((p) => this.LightDarkMode = (LightDarkMode)p);
+            this.ApplyPrimaryCommand = new ActionCommand((o) => this.PrimarySwatch = (Swatch)o);
+            this.ApplyAccentCommand = new ActionCommand((o) => this.AccentSwatch = (Swatch)o);
+
+            this.PrimarySwatches = new SwatchesProvider().Swatches.Select(s => new SwatchViewModel(s, this.ApplyPrimaryCommand));
+            this.AccentSwatches = new SwatchesProvider().Swatches.Select(s => new SwatchViewModel(s, this.ApplyAccentCommand));
         }
-        public ICommand SetThemeCommand => new ActionCommand((p) => this.LightDarkMode = (LightDarkMode)p);
+        
         public LightDarkMode LightDarkMode
         {
             get => this.lightDarkMode;
@@ -34,20 +42,56 @@ namespace MoSeqAcquire.ViewModels
                 this.SetField(ref this.lightDarkMode, value);
             }
         }
-
-        public IEnumerable<Swatch> Swatches { get; }
-        public ICommand ApplyPrimaryCommand { get; } = new ActionCommand(o => ApplyPrimary((Swatch)o));
-
-        private static void ApplyPrimary(Swatch swatch)
+        public Swatch PrimarySwatch
         {
-            new PaletteHelper().ReplacePrimaryColor(swatch);
+            get => this.primarySwatch;
+            set
+            {
+                this.helper.ReplacePrimaryColor(value);
+                this.PrimarySwatches.ForEach(s => s.IsSelected = false);
+                this.PrimarySwatches.Where(s => s.Swatch.Equals(value)).ForEach(s => s.IsSelected = true);
+                this.SetField(ref this.primarySwatch, value);
+            }
+        }
+        public Swatch AccentSwatch
+        {
+            get => this.accentSwatch;
+            set
+            {
+                this.helper.ReplaceAccentColor(value);
+                this.AccentSwatches.ForEach(s => s.IsSelected = false);
+                this.AccentSwatches.Where(s => s.Swatch.Equals(value)).ForEach(s => s.IsSelected = true);
+                this.SetField(ref this.accentSwatch, value);
+            }
         }
 
-        public ICommand ApplyAccentCommand { get; } = new ActionCommand(o => ApplyAccent((Swatch)o));
+        public IEnumerable<SwatchViewModel> PrimarySwatches { get; }
+        public IEnumerable<SwatchViewModel> AccentSwatches { get; }
+        public ICommand SetThemeCommand { get; protected set; }
+        public ICommand ApplyPrimaryCommand { get; protected set; }
+        public ICommand ApplyAccentCommand { get; protected set; }
+    }
 
-        private static void ApplyAccent(Swatch swatch)
+    public class SwatchViewModel : BaseViewModel
+    {
+        protected Swatch swatch;
+        protected bool isSelected;
+        public SwatchViewModel(Swatch swatch)
         {
-            new PaletteHelper().ReplaceAccentColor(swatch);
+            this.swatch = swatch;
         }
+        public SwatchViewModel(Swatch swatch, ICommand selectionCommand)
+        {
+            this.swatch = swatch;
+            this.SelectionCommand = selectionCommand;
+        }
+        public Swatch Swatch { get => this.swatch; }
+        public string Name { get => this.swatch.Name; }
+        public bool IsSelected
+        {
+            get => this.isSelected;
+            set => this.SetField(ref this.isSelected, value);
+        }
+        public ICommand SelectionCommand { get; set; }
     }
 }
---
+++ MoSeqAcquire/Views/Converters/ObjectEqualityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+using System.Reflection;
+
+namespace MoSeqAcquire.Views.Converters
+{
+    public class ObjectEqualityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string parameterString = parameter as string;
+            if (parameter == null)
+                return DependencyProperty.UnsetValue;
+
+            return parameter.Equals(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
