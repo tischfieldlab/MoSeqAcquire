386faa850418762c7cad8951de3e99b5894bc505
quantumdot <thackray@rutgers.edu>
2018-04-25 11:20:01 -0400

    .

--- MoSeqAcquire/Models/Acquisition/Kinect/KinectConfig.cs
+++ MoSeqAcquire/Models/Acquisition/Kinect/KinectConfig.cs
@@ -514,4 +514,94 @@ namespace MoSeqAcquire.Models.Acquisition.Kinect
         }
         #endregion
     }
+
+    class RangedKinectPropertyItem<T> : ComplexProperty
+    {
+        KinectManager source;
+        string valueProperty;
+        string minProperty;
+        string maxProperty;
+        T currentValue;
+
+        public RangedKinectPropertyItem(KinectManager Source, string valueProperty, string minProperty, string maxProperty)
+        {
+            this.valueProperty = valueProperty;
+            this.minProperty = minProperty;
+            this.maxProperty = maxProperty;
+        }
+
+        public override object Value
+        {
+            get
+            {
+                this.ReadCurrentValue();
+                return this.currentValue;
+            }
+            set
+            {
+                this.currentValue = (T)value;
+                this.PushCurrentValue();
+            }
+        }
+        public override bool IsAutomatic { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void ResetValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void PushCurrentValue()
+        {
+            this.SetPropertyValue(this.source.Sensor, this.valueProperty, this.currentValue);
+        }
+        protected override void ReadCurrentValue()
+        {
+            this.currentValue = (T)this.GetPropertyValue(this.source.Sensor, this.valueProperty);
+        }
+
+        protected override PropertyCapability ReadCapability()
+        {
+            return new RangedPropertyCapability()
+            {
+                Min = this.GetPropertyValue(this.source.Sensor, this.minProperty),
+                Max = this.GetPropertyValue(this.source.Sensor, this.maxProperty)
+            };
+        }
+        public object GetPropertyValue(object src, string propName)
+        {
+            if (src == null) throw new ArgumentException("Value cannot be null.", "src");
+            if (propName == null) throw new ArgumentException("Value cannot be null.", "propName");
+
+            if (propName.Contains("."))//complex type nested
+            {
+                var temp = propName.Split(new char[] { '.' }, 2);
+                return GetPropertyValue(GetPropertyValue(src, temp[0]), temp[1]);
+            }
+            else
+            {
+                var prop = src.GetType().GetProperty(propName);
+                return prop != null ? prop.GetValue(src, null) : null;
+            }
+        }
+        public void SetPropertyValue(object src, string propName, object value)
+        {
+            if (src == null) throw new ArgumentException("Value cannot be null.", "src");
+            if (propName == null) throw new ArgumentException("Value cannot be null.", "propName");
+
+            if (propName.Contains("."))//complex type nested
+            {
+                var temp = propName.Split(new char[] { '.' }, 2);
+                SetPropertyValue(GetPropertyValue(src, temp[0]), temp[1], value);
+            }
+            else
+            {
+                var prop = src.GetType().GetProperty(propName);
+                if (prop != null)
+                {
+                    prop.SetValue(src, value);
+                }
+            }
+        }
+
+    }
 }
