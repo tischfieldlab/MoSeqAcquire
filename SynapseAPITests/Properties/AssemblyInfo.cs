c73d715b10c259a7d252edfb54c58c6366383da3
quantumdot <thackray@rutgers.edu>
2019-02-25 18:54:50 -0500

    much work on Synapse REST API client, including some unit tests

---
+++ SynapseAPITests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SynapseAPITests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Division of Life Sciences")]
+[assembly: AssemblyProduct("SynapseAPITests")]
+[assembly: AssemblyCopyright("Copyright © Division of Life Sciences 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4264a131-4d1b-46ea-8743-3aea3844be36")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
