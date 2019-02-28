de03b4bdd18f7d3413c048d30463a0be68597594
quantumdot <thackray@rutgers.edu>
2019-02-28 14:43:51 -0500

    a little cleanup

--- MoSeqAcquire/App.xaml.cs
+++ MoSeqAcquire/App.xaml.cs
@@ -20,12 +20,10 @@ namespace MoSeqAcquire
 
         public App()
         {
-
             Settings.Default.MediaSourcePluginPaths.Cast<string>().ForEach((p) => RegisterProbePath(p));
             Settings.Default.RecorderPluginPaths.Cast<string>().ForEach((p) => RegisterProbePath(p));
 
             AppDomain.CurrentDomain.AssemblyResolve += LoadResolveEventHandler;
-
         }
 
         private static List<String> __privateProbPaths = new List<String>();
@@ -60,75 +58,5 @@ namespace MoSeqAcquire
             }
             return null;
         }
-        /*
-        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            // missing resources are... missing
-            if (args.Name.Contains(".resources"))
-                return null;
-
-            // check for assemblies already loaded
-            Assembly assembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.FullName == args.Name);
-            if (assembly != null)
-                return assembly;
-
-            // Try to load by filename - split out the filename of the full assembly name
-            // and append the base path of the original assembly (ie. look in the same dir)
-            // NOTE: this doesn't account for special search paths but then that never
-            //           worked before either.
-            string filename = args.Name.Split(',')[0] + ".dll".ToLower();
-
-
-            // try load the assembly from install path (this should always be automatic)
-            try
-            {
-                // this allows addins to load before form has loaded
-                return Assembly.LoadFrom(filename);
-            }
-            catch
-            {
-            }
-
-            var search_paths = new List<string>();
-            search_paths.AddRange(Settings.Default.MediaSourcePluginPaths.Cast<string>().ToArray());
-            search_paths.AddRange(Settings.Default.RecorderPluginPaths.Cast<string>().ToArray());
-
-            foreach (var sp in search_paths)
-            {
-                // try load from install addins folder
-                string asmFile = FindFileInPath(filename, sp);
-                if (!string.IsNullOrEmpty(asmFile))
-                {
-                    try
-                    {
-                        return Assembly.LoadFrom(asmFile);
-                    }
-                    catch
-                    {
-                    }
-                }
-            }
-
-            return null;
-        }
-        private string FindFileInPath(string filename, string path)
-        {
-            filename = filename.ToLower();
-
-            foreach (var fullFile in Directory.GetFiles(path))
-            {
-                var file = Path.GetFileName(fullFile).ToLower();
-                if (file == filename)
-                    return fullFile;
-            }
-            foreach (var dir in Directory.GetDirectories(path))
-            {
-                var file = FindFileInPath(filename, dir);
-                if (!string.IsNullOrEmpty(file))
-                    return file;
-            }
-
-            return null;
-        }*/
     }
 }
